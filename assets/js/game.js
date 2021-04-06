49b30d069b737e429434acdeaf58de93bb461b3d
Ronald Main <rbmain84@yahoo.com>
2021-04-06 14:41:38 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,18 +4,14 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
-
+    while (enemyHealth > 0)
+  
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
@@ -23,9 +19,6 @@ var fight = function(enemyName) {
   if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
-    console.log(
-      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-    );
 
     // check enemy's health
     if (enemyHealth <= 0) {
@@ -36,9 +29,6 @@ var fight = function(enemyName) {
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
-    console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
 
     // check player's health
     if (playerHealth <= 0) {
@@ -66,6 +56,7 @@ var fight = function(enemyName) {
     window.alert("You need to pick a valid option. Try again!");
   }
 };
+
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
